467517b6c6e88376e28b8ef0cbdaa20cc1f57510
angeldollface <112771657+angeldollface@users.noreply.github.com>
2023-06-26 22:56:47 +0000

    Deploying to gh-pages from @ angeldollface/mandys-house@d13388b72c07189aaf5728af54dfdeaba5e603df 🚀

---
+++ assets/javascript/nav.js
@@ -0,0 +1,11 @@
+function openNav(id){
+    let elem = document.getElementById(id);
+    elem.style.width = '100vw';
+    elem.style.height = '100vh';
+}
+
+function closeNav(id){
+    let elem = document.getElementById(id);
+    elem.style.width = '0vw';
+    elem.style.height = '0vh';
+}
