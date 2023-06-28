f4c839e3ce1050aad7b3b27e22cfffe07477e0a6
angeldollface <112771657+angeldollface@users.noreply.github.com>
2023-06-28 20:18:39 +0000

    Deploying to gh-pages from @ angeldollface/mandys-house@7b6910fcd0107a4340d22b6cd0609505a34e12c5 🚀

---
+++ installers/win_64.ps1
@@ -0,0 +1,16 @@
+# MANDY WINDOWS INSTALLER 
+# by Alexander Abraham a.k.a. "Angel Dollface".
+# Licensed under the MIT license.
+
+# Install the Rust toolchain if it isn't installed.
+if(Get-Command cargo){
+    Write-Output "Required toolchain installed, proceeding."
+}
+else {
+    Invoke-WebRequest -URI https://win.rustup.rs/x86_64 -OutFile rustup.exe
+    ./rustup.exe
+}
+
+# Install Mandy.
+cargo install --git https://github.com/angeldollface/mandy --force
+
