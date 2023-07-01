8f19a726941dbcf32a50523ff72bd2604ea79dd1
✭ ANGEL DOLLFACE ✭ <112771657+angeldollface@users.noreply.github.com>
2023-07-01 23:18:48 +0200

    Update nix.sh

--- installers/nix.sh
+++ installers/nix.sh
@@ -4,7 +4,7 @@
 
 # Install the Rust toolchain if it isn't installed.
 if ! [ -x "$(command -v cargo)" ]; then 
-    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 else
     echo "Required toolchain installed, proceeding."
 fi
