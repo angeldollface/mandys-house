a29eef186df0790f104034b6bd2d88a193e32136
angeldollface <112771657+angeldollface@users.noreply.github.com>
2023-07-10 16:15:29 +0000

    Deploying to gh-pages from @ angeldollface/mandys-house@31408a6643d9cf35bb9817acb3a4de9d78ae2ea9 🚀

--- installers/win_32.ps1
+++ installers/win_32.ps1
@@ -12,4 +12,4 @@ else {
 }
 
 # Install Mandy.
-cargo install --git https://github.com/angeldollface/mandy --force --tag v.0.2.0
+cargo install --git https://github.com/angeldollface/mandy mandy-bin --force --tag v.0.3.0
