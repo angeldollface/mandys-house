3d74146d1c3f7e3b4e15b87178f231ef2e98ae13
✭ ANGEL DOLLFACE ✭ <112771657+angeldollface@users.noreply.github.com>
2023-07-14 20:26:30 +0200

    Update win_32.ps1

--- installers/win_32.ps1
+++ installers/win_32.ps1
@@ -12,4 +12,4 @@ else {
 }
 
 # Install Mandy.
-cargo install --git https://github.com/angeldollface/mandy mandy-bin --force --tag v.0.3.0
+cargo install --git https://github.com/angeldollface/mandy mandy-bin --force --tag v.0.3.1
