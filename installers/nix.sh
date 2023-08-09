ce1cf45fbdd0b0cb504f9f264f8b38f503e078c3
angeldollface <112771657+angeldollface@users.noreply.github.com>
2023-08-09 14:42:08 +0000

    Deploying to gh-pages from @ angeldollface/mandys-house@f6af5081686dc37c05c572179491ffd3657efe90 🚀

--- installers/nix.sh
+++ installers/nix.sh
@@ -10,4 +10,4 @@ else
 fi
 
 # Install Mandy.
-cargo install --git https://github.com/angeldollface/mandy mandy-bin --force --tag v.0.3.2
+cargo install --git https://github.com/angeldollface/mandy mandy-bin --force --tag v.0.3.3
